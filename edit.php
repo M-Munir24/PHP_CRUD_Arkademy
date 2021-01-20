755f11c879d702a4e89242416f762e645552a262
m-munir24 <52531952+M-Munir24@users.noreply.github.com>
2021-01-20 18:18:39 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -39,32 +39,37 @@ while($user_data = mysqli_fetch_array($result))
 <html>
 <head>  
     <title>Edit Produk</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <a href="index.php">Home</a>
+<h1 class="display-4 mt-5">Edit Product</h1>
+
+    <a href="index.php">< Home</a>
     <br/><br/>
 
     <form name="update_user" method="post" action="edit.php">
+    
         <table>
         
             <tr> 
                 <td>Nama Produk</td>
-                <td><input type="text" name="nama_produk" value='<?php echo $nama_produk;?>'></td>
+                <td><input class="form-control" type="text" name="nama_produk" value='<?php echo $nama_produk;?>'></td>
             </tr>
             
             <tr> 
                 <td>Keterangan</td>
-                <td><input type="text"  name="keterangan" value=<?php echo $keterangan;?>></td>
+                <td><textarea class="form-control" type="text"  name="keterangan"><?php echo $keterangan;?></textarea></td>
             </tr>
             <tr> 
                 <td>Harga</td>
-                <td><input type="text" name="harga" value='<?php echo $harga;?>'></td>
+                <td><input class="form-control" type="text" name="harga" value='<?php echo $harga;?>'></td>
             </tr>
            
              <tr> 
                 <td>Jumlah</td>
-                <td><input type="text"  name="jumlah" value='<?php echo $jumlah;?>'></td>
+                <td><input class="form-control" type="text"  name="jumlah" value='<?php echo $jumlah;?>'></td>
             </tr>
             <tr>
                 <td><input type="hidden" name="id" value='<?php echo $_GET['id'];?>'></td>
