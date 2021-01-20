98128b3440647079e7b538c6d3135b7e069f4630
m-munir24 <52531952+M-Munir24@users.noreply.github.com>
2021-01-20 18:17:54 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,10 +1,13 @@
 <html>
 <head>
-    <title>Add Mhs</title>
+    <title>Add Product</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
 </head>
 
 <body>
-    <a href="index.php">Go to Home</a>
+    <h1 class="display-4 mt-5">Tambah Product</h1>
+    <a href="index.php">< Home</a>
     <br/><br/>
 
     <form action="add.php" method="post" name="form1">
@@ -12,25 +15,30 @@
           
             <tr> 
                 <td>Nama Produk</td>
-                <td><input type="text" name="nama_produk"></td>
+                <td> :</td>
+                <td><input class="form-control" type="text" name="nama_produk"></td>
             </tr>
             <tr> 
                 <td>Keterangan</td>
-                <td><input type="text" name="keterangan"></td>
+                <td> :</td>
+                <td><textarea class="form-control" type="text" name="keterangan"></textarea></td>
             </tr>
              <tr> 
                 <td>harga</td>
-                <td><input type="text" name="harga"></td>
+                <td> :</td>
+                <td><input class="form-control" type="text" name="harga"></td>
             </tr>
 
             <tr> 
                 <td>jumlah</td>
-                <td><input type="text" name="jumlah"></td>
+                <td> :</td>
+                <td><input class="form-control" type="text" name="jumlah"></td>
             </tr>
             
             <tr> 
                 <td></td>
-                <td><input type="submit" name="Submit" value="Add"></td>
+                <td></td>
+                <td><button type="submit" class="btn btn-success btn-sm btn-block" name="Submit">Submit</button></td>
             </tr>
         </table>
     </form>
@@ -52,7 +60,10 @@
         $result = mysqli_query($mysqli, "INSERT INTO produk(nama_produk,keterangan,harga,jumlah) VALUES('$nama_produk','$keterangan','$harga','$jumlah')");
 
         // Show message when user added
-        echo "User added successfully. <a href='index.php'>View user</a>";
+        // echo "User added successfully. <a href='index.php'>View Produk</a>";
+        header("Location:index.php");
+
+        
     }
     ?>
 </body>
